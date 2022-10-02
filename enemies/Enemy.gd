33d2951f2a9ae02bf2d7e1a119b50102f122375c
AndreaTerenz <andrea.terenziani.at@gmail.com>
2022-10-03 00:02:14 +0200

    Improved enemy roaming anim
    I think?

--- enemies/Enemy.gd
+++ enemies/Enemy.gd
@@ -2,16 +2,17 @@ class_name Enemy
 extends KinematicBody2D
 
 enum STATES {
+	SPAWN,
 	ROAM,  # fly around until player is in reach
 	ATTACK # go fuck up the player
 }
 
 export(float, 2.0, 10.0, 0.1) var attack_dist = 5.0
-export(float, 5.0, 200.0, 0.1) var roam_jitter = 100.0
 export(float, 1.0, 500.0, 0.1) var roam_max_speed = 100.0
 export(float, 0.01, 1.0, 0.01) var roam_acceleration = 0.4
 
 onready var ray := $RayCast2D
+onready var anim_sprite := $AnimatedSprite
 
 var noise_idx := 0.0
 var noise := OpenSimplexNoise.new()
@@ -21,7 +22,8 @@ var velocity := Vector2.ZERO
 var dir := Vector2.RIGHT
 
 func _ready():
-	$AnimatedSprite.playing = true
+	anim_sprite.animation = "spawning"
+	anim_sprite.playing = true
 	# Since there are no enemies during the day,
 	# we can assume this node will be instanced
 	# WAY AFTER the whole scene has been loaded
@@ -31,6 +33,7 @@ func _ready():
 		player = owner.player 
 	
 	# Configure noise
+	randomize()
 	noise.seed = randi()
 	noise.octaves = 2
 	noise.period = 20.0
@@ -38,11 +41,15 @@ func _ready():
 
 
 func _physics_process(delta):
+	state = STATES.ROAM
+	
 	check_state()
 	run_state(delta)
 	
 func check_state():
-	state = STATES.ROAM
+	if anim_sprite.animation == "spawning":
+		state = STATES.SPAWN
+		return 
 		
 	if not player:
 		return
@@ -63,12 +70,15 @@ func check_state():
 	
 func run_state(delta):
 	match state:
+		STATES.SPAWN:
+			# nothing to do
+			pass
 		STATES.ROAM:
 			noise_idx += delta * 100
 			var _n = noise.get_noise_1d(noise_idx)
 			var n = range_lerp(_n, -1.0, 1.0, -TAU/4.0, TAU/4.0)
 			
-			dir = lerp(dir, dir.rotated(n * 0.1), 0.5)
+			dir = dir.linear_interpolate(dir.rotated(n * 0.1), 0.5)
 			
 			#print("noise: %s | angle: %s | dir: %s " % [_n, rad2deg(n), dir])
 			
@@ -76,11 +86,23 @@ func run_state(delta):
 			
 			var coll = move_and_collide(velocity * delta)
 			
+			var coll_horizontal := false
 			if coll:
-				dir = dir.rotated(TAU/2.0)
-				velocity = velocity.rotated(TAU/2.0)
-			
-			$AnimatedSprite.flip_h = (velocity.x > 0)
-
+				var norm = coll.normal
+				if norm.y == 0:
+					# vertical wall
+					dir.x *= -1
+					velocity.x *= -1
+				else:
+					dir.y *= -1
+					velocity.y *= -1
+					
+			anim_sprite.flip_h = (velocity.x > 0)
 		STATES.ATTACK:
 			pass
+
+
+func _on_AnimatedSprite_animation_finished():
+	if anim_sprite.animation == "spawning":
+		anim_sprite.animation = "roaming"
+		anim_sprite.playing = true
